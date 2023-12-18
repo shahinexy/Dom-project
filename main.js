fcc9a1f6f584095a751dc00a5a2a36f00f577be3
shahinexy <shahinexy@gmail.com>
2023-12-18 23:41:26 +0600

    tabb content work done

--- main.js
+++ main.js
@@ -12,3 +12,18 @@ showHid.addEventListener('click', ()=>{
 })
 
 // tab section 
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e)=>{
+    if(e.target.tagName == 'LI'){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach((panel) => {
+            if(panel == targetPanel){
+                panel.classList.add('active');
+            } else{
+                panel.classList.remove('active');
+            }
+        });
+    }
+})
+
