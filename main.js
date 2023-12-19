be54152c27b0ecf83f66089a45644bf68834c0d7
shahinexy <shahinexy@gmail.com>
2023-12-19 13:46:28 +0600

    todo list js work done

--- main.js
+++ main.js
@@ -29,11 +29,39 @@ tabs.addEventListener('click', (e)=>{
 
 
 // == Search section ==
+// delete button
 const ul = document.querySelector('.search_area ul');
 ul.addEventListener('click', (e)=>{
     if(e.target.classList.contains('delete')){
         const li = e.target.parentElement;
         li.parentElement.removeChild(li);
     }
+})
+
+// - adding list - 
+const addList = document.forms['add_list'];
+addList.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const value = e.target.querySelector('input[type ="text"]').value;
+
+    // create element 
+    const newLi = document.createElement('li');
+    const newP = document.createElement('p');
+    const newBtn = document.createElement('button');
+
+    // add content 
+    newP.innerText = value;
+    newBtn.innerText = 'delete';
+
+    // add class
+    newLi.classList.add('flex', 'gap-5', 'my-2')
+    newP.classList.add('w-5/6', 'text-xl', 'border-b-2','border-purple-400')
+    newBtn.classList.add('delete', 'w-1/6','text-xl', 'p-1', 'rounded', 'bg-purple-600')
+
+    // append child
+    newLi.appendChild(newP);
+    newLi.appendChild(newBtn);
+    ul.appendChild(newLi);
+
 })
 
